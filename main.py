d6760c65cfdac1bee9ba5121e41f251891a51fbf
Abisha Ebinesar <116267549+Abisha-2000@users.noreply.github.com>
2023-05-26 13:15:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-import streamlit as st
+from textblob import TextBlob
 import pandas as pd
-import numpy as np
-import TextBlob
+import streamlit as st
 import cleantext
 
-st.write ("Sentiment Analysis")
 
-file = st.file_uploader("Pick a file")
-
-if file is not None:
-    df= pd.read_csv(file)
-    st.write(df)
-
-
-    st.write("The Description of the file selected")
-    st.write(df.describe())
-    
-    column = st.selectbox("Select a column name : ",np.array(df.columns))
-    
-    st.write("Description of the selected column")
-    st.write(df[column].describe())
-
-    if(len(df[column].unique()) < 10):
-        st.write("The Unique Values in the selected column")
-        st.write(df[column].unique())
-       
 st.header('Sentiment Analysis')
 with st.expander('Analyze Text'):
     text = st.text_input('Text here: ')
@@ -76,6 +55,8 @@ with st.expander('Analyze CSV'):
             file_name='sentiment.csv',
             mime='text/csv',
         )
+
+
 
 
 

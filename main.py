f4c41a626043431e68c3333ec3cf02f406a28dac
Abisha Ebinesar <116267549+Abisha-2000@users.noreply.github.com>
2023-05-31 12:49:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
-  st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
-
+  
 

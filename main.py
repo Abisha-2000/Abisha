b73575fce457f5e6234a87489ceb713ea9900d83
Abisha Ebinesar <116267549+Abisha-2000@users.noreply.github.com>
2023-05-31 13:15:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from textblob import TextBlob
+
 
 
 st.title("SENTIMENTAL ANALYSIS!")
@@ -10,11 +10,5 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
 
-if st.button("Analyze the Sentiment"):
-	
-blob = TextBlob(Twitter)
-result = blob.sentiment
-polarity = result.polarity
-subjectivity = result.subjectivity
 
 
